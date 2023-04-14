b314d12dcd5314a036a17c41843dc7ef483ae87a
Angelica Pereira <angelica@Angelicas-MBP.fritz.box>
2023-04-14 15:26:40 +0200

    Editing the comments and paramenters

--- random_number_printer.py
+++ random_number_printer.py
@@ -1,22 +1,22 @@
 import argparse
 import random
 
-# Create a parser object to handle command-line arguments
+
 parser = argparse.ArgumentParser(description='Print numbers from 1 to 10 in random order.')
 
-# Add an optional argument for the random seed
+
 parser.add_argument('--seed', type=int, help='Random seed', default=None)
 
-# Parse the arguments
+
 args = parser.parse_args()
 
-# Set the random seed if provided
+
 if args.seed:
     random.seed(args.seed)
 
-# Generate a list of numbers from 1 to 10 in random order
+
 numbers = list(range(1, 11))
 random.shuffle(numbers)
 
-# Print the numbers
+
 print(numbers)
